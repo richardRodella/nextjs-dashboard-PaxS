78fb8ffdad7a2916c7083371e48d30630c5156a3
PaxSomnius <git@paxsomnius.com>
2024-01-03 12:36:55 +0000

    init Commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter } from 'next/font/google';
+import { Roboto_Serif } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const robotoSerif = Roboto_Serif({ subsets: ['latin'], display: 'swap', weight: ['400','600'] });
